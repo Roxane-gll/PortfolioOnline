84d11160c1d37ec8f53069a4b0faaf662b1f3312
Roxane-gll <Administrateur@TESLA_A04.formationara.local>
2021-03-26 14:48:24 +0100

    Remove the now ignored directory node_modules

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app=express();
-const http = require('http').Server(app);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 app.use(express.static('public'));
-
-http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
