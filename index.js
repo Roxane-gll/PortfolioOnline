2195e91517675ec1ae269c7bc6481dceca61bc5a
Roxane-gll <Administrateur@TESLA_A04.formationara.local>
2021-03-26 15:01:04 +0100

    test heroku v6

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app=express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 app.use(express.static('public'));
+
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
