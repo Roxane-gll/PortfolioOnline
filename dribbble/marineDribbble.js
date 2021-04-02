a7ccad731279af3c5a627518f4478bb4b27260b2
Roxane-gll <guella.roxane@gmail.com>
2021-04-02 15:55:55 +0200

    adding css, adding link in contact and adding modal

--- dribbble/marineDribbble.js
+++ dribbble/marineDribbble.js
@@ -1,5 +1,10 @@
 export let accessToken=
     '9e8a4bb338017ba91bd43e74e0c34d144c7a675fe28af59cd92c3394afe96143';
 
-export let name='Marine';
-export let tel='Marine Mousset';
+export let name=' Vous voulez en apprendre plus ? <br>Je serai ravie de vous répondre et d’essayer de construire un projet ensemble !<br>\n' +
+    '<a href="mailto:marine-mousset@laposte.net">Mail</a><br>\n' +
+    '<a href="https://www.instagram.com/mari.mous.art/">Instagram</a><br>\n' +
+    '<a href="https://fr.linkedin.com/in/marine-mousset-710885203">Linkedin</a>';
+export let aPropos=' Bonjour ! Moi c’est Marine, je suis étudiante aux Gobelins comme designer web et mobile mais je suis aussi passionnée par le domaine du print et de l’illustration.<br>\n' +
+    'Ce que je souhaite, c’est faire rêver les personnes et développer leur imaginaire à travers des créations visuelles pétillantes et poétiques.<br>\n' +
+    'Êtes vous prêt ? Je vous emmène avec moi pour vous faire découvrir mon univers !\n';
--- portfolio.js
+++ portfolio.js
@@ -8,36 +8,50 @@ import * as marine from './dribbble/marineDribbble.js';
     person='roxane';
 }*/
 
-let person=roxane;
+let person=marine;
 
 let contactsPerson=document.getElementById('contact')
-contactsPerson.innerHTML=`<p>${person.name}`
+contactsPerson.innerHTML=`<div><p>${person.name}</div>`
 
 let aProposPerson=document.getElementById('aPropos')
-aProposPerson.innerHTML=`<p>${person.tel}`
+aProposPerson.innerHTML=`<div><p>${person.aPropos}</div>`
 
 const buttonsAllPage= document.querySelectorAll(".buttonInAllPage");
+
+const modal=document.querySelector("#modal");
 
 for ( let buttonAllPage of  buttonsAllPage) {
     buttonAllPage.addEventListener("click",()=>{
         if(buttonAllPage.value==='contacts'){
             buttonAllPage.nextElementSibling.classList.toggle("hiddenC")
             buttonAllPage.nextElementSibling.classList.toggle("visibleC")
+            modal.classList.toggle("modalOn")
+            modal.classList.toggle("modalOff")
+            buttonAllPage.style.zIndex=2
             buttonAllPage.innerText='Close';
             buttonAllPage.value='contactClose';
         }else if(buttonAllPage.value==='contactClose'){
             buttonAllPage.nextElementSibling.classList.toggle("hiddenC")
             buttonAllPage.nextElementSibling.classList.toggle("visibleC")
+            modal.classList.toggle("modalOn")
+            modal.classList.toggle("modalOff")
+            buttonAllPage.style.zIndex=1
             buttonAllPage.innerText='Contacts';
             buttonAllPage.value='contacts';
         }else if(buttonAllPage.value==='aPropos'){
             buttonAllPage.nextElementSibling.classList.toggle("hiddenP")
             buttonAllPage.nextElementSibling.classList.toggle("visibleP")
+            modal.classList.toggle("modalOn")
+            modal.classList.toggle("modalOff")
+            buttonAllPage.style.zIndex=2
             buttonAllPage.innerText='Close';
             buttonAllPage.value='aProposClose';
         }else if(buttonAllPage.value==='aProposClose'){
             buttonAllPage.nextElementSibling.classList.toggle("hiddenP")
             buttonAllPage.nextElementSibling.classList.toggle("visibleP")
+            modal.classList.toggle("modalOn")
+            modal.classList.toggle("modalOff")
+            buttonAllPage.style.zIndex=0
             buttonAllPage.innerText='À propos';
             buttonAllPage.value='aPropos';
         }
@@ -51,13 +65,68 @@ $.ajax({
     success: function(data) {
         if (data.length > 0) {
             $.each(data.reverse(), function(i, val) {
-                $('#projets').prepend(
-                    '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
-                )
+                if(val.title.indexOf('#0')){
+                    $('#projets').prepend(
+                        '<div id="'+val.title.slice(0,-2)+'" class="carousel slide" data-ride="carousel">\n' +
+                        '<ol class="carousel-indicators" id="'+val.title.slice(0,-2)+'-indicators">\n' +
+                        '    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>\n' +
+                        '</ol>\n'+
+                        '<div class="carousel-inner" id="'+val.title.slice(0,-2)+'-inner">\n'+
+                        '   <div class="carousel-item active">\n' +
+                        '        <a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>\n' +
+                        '   </div>\n'+
+                        '<a class="carousel-control-prev" href="#no-shots" role="button" data-slide="prev">\n' +
+                        '       <span class="carousel-control-prev-icon" aria-hidden="true"><</span>\n' +
+                        '       <span class="sr-only">Previous</span>\n' +
+                        '   </a>\n' +
+                        '   <a class="carousel-control-next" href="#no-shots" role="button" data-slide="next">\n' +
+                        '       <span class="carousel-control-next-icon" aria-hidden="true"> > </span>\n' +
+                        '       <span class="sr-only">Next</span>\n' +
+                        '   </a>\n' +
+                        '</div>\n'
+                    )
+                }else {
+                    $('<div class="carousel-item">\n' +
+                        '        <a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>\n' +
+                        '   </div>').appendTo('#'+val.title.slice(0,-2)+'-inner');
+                    $('<li data-target="#carouselExampleIndicators" data-slide-to="'+val.title.slice(-1)+'"></li>').appendTo('#'+val.title.slice(0,-2)+'-indicators')
+
+                }
             })
         }
         else {
-            $('#projets').append('<p>No shots yet!</p>');
+            $('#projets').append(
+                '<div id="no-shots" class="carousel slide" data-ride="carousel">\n' +
+                '<div class="carousel-inner">\n'+
+                '   <div class="carousel-item active">\n' +
+                '        <p class="car">no shots</p>\n' +
+                '   </div>\n'+
+                '<a class="carousel-control-prev" href="#no-shots" role="button" data-slide="prev">\n' +
+                '       <span class="carousel-control-prev-icon" aria-hidden="true"><</span>\n' +
+                '       <span class="sr-only">Previous</span>\n' +
+                '   </a>\n' +
+                '   <a class="carousel-control-next" href="#no-shots" role="button" data-slide="next">\n' +
+                '       <span class="carousel-control-next-icon" aria-hidden="true"> > </span>\n' +
+                '       <span class="sr-only">Next</span>\n' +
+                '   </a>\n' +
+                '</div>\n'
+            );
         }
     }
 });
+
+
+// API Variables
+
+//Image width 800x600 (Animated)
+// val.images.hidpi
+//Image width 400x300
+// val.images.normal
+//Image width 200x150
+// val.images.teaser
+//Title
+// val.title
+//Description
+// val.description
+//URL
+// val.html_url
