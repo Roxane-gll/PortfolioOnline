1cf898e35ca2aa8c584e97bd89dbc0fabe0c6ad1
Roxane-gll <guella.roxane@gmail.com>
2021-04-09 16:14:32 +0200

    testing heroku env var v3

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $access_token=getenv('accessToken');
 
 ?>
 
-<script type="text/javascript">let access=<? $access_token ?></script>
+<script type="text/javascript">let access=<? $access_token=getenv('accessToken') ?></script>
 <script type="text/javascript" src="portfolio.js"></script>
